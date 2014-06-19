3f047828e448a8c32232792861d2949172d9e4b8
Vegard Angell <v.angell123@gmail.com>
2014-06-19 12:32:54 +0200

    added basic admin view. nothing fancy (yet)

---
+++ cradmin_demo/cradmin_demo/webdemo/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from cradmin_demo.webdemo.models import Site, Page
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display=('name', 'admins_as_string')
+
+    def admins_as_string(self, obj):
+        return ', '.join([user.username for user in obj.admins.all()])
+    admins_as_string.short_description="Admins"
+
+admin.site.register(Site, SiteAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display=('title',)
+
+admin.site.register(Page, PageAdmin)
