eca03c97942e1482ac23a7af0def12ef17896802
Espen Angell Kristiansen <post@espenak.net>
2014-06-26 00:09:57 +0200

    Cleanup the acemarkdown widget code.

--- django_cradmin/acemarkdown/widgets.py
+++ django_cradmin/acemarkdown/widgets.py
@@ -1,9 +1,14 @@
+import json
 from django import forms
 from django.template.loader import render_to_string
 
 
 class AceMarkdownWidget(forms.widgets.Textarea):
     template_name = 'django_cradmin/acemarkdown/widget.django.html'
+    directiveconfig = {
+        # 'showTextarea': False,
+        # 'theme': 'tomorrow'
+    }
 
     @property
     def media(self):
@@ -19,6 +24,7 @@ class AceMarkdownWidget(forms.widgets.Textarea):
         textarea = super(AceMarkdownWidget, self).render(name, value, attrs)
         return render_to_string(
             self.template_name, {
-                'textarea': textarea
+                'textarea': textarea,
+                'directiveconfig': json.dumps(self.directiveconfig)
             }
         )
