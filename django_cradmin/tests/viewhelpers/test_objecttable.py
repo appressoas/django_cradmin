8a4ded0767c1cb0e87b274dbc498e56b9d5d9da4
Espen Angell Kristiansen <post@espenak.net>
2014-07-20 17:11:55 +0200

    Fixed the objecttable tests.

--- django_cradmin/tests/viewhelpers/test_objecttable.py
+++ django_cradmin/tests/viewhelpers/test_objecttable.py
@@ -2,20 +2,22 @@ import mock
 from django.test import TestCase
 from django.db import models
 
-from django_cradmin.viewhelpers.objecttable import Column, PlainTextColumn, SingleActionColumn, \
-                                                   MultiActionColumn, Button
+from django_cradmin.viewhelpers import objecttable
+
 
 class TestColumn(TestCase):
     def setUp(self):
         class TestModel(models.Model):
             testfield = models.CharField(verbose_name=u'Test Value')
+
             class Meta:
                 app_label = 'django_cradmin.viewhelpers_test'
 
-        class TestColSubclass(Column):
+        class TestColSubclass(objecttable.Column):
             modelfield = 'testfield'
+
             def __init__(self, **kwargs):
-                super(TestColSubclass,self).__init__(**kwargs)
+                super(TestColSubclass, self).__init__(**kwargs)
 
         self.model_testobject = TestModel()
         view = mock.MagicMock()
@@ -24,7 +26,7 @@ class TestColumn(TestCase):
         self.column_subclass_noview = TestColSubclass()
 
     def test_get_header_not_implemented(self):
-        test = Column()
+        test = objecttable.Column()
 
         with self.assertRaises(NotImplementedError):
             test.get_header()
@@ -42,29 +44,32 @@ class TestColumn(TestCase):
 
     # check that you get an exception when running render_value without having modelfield..
     def test_render_value_not_implemented(self):
-        col = Column()
+        col = objecttable.Column()
 
         with self.assertRaises(NotImplementedError):
             col.render_value(None)
 
     # check that you get an exception when running render_cell without overriding with subclass..
     def test_render_cell_not_implemented(self):
-        col = Column()
+        col = objecttable.Column()
 
         with self.assertRaises(NotImplementedError):
             col.render_cell(None)
+
 
 class TestPlainTextColumn(TestCase):
     def setUp(self):
         class TestModel(models.Model):
             testfield = models.CharField(verbose_name=u'Test Value')
+
             class Meta:
                 app_label = 'django_cradmin.viewhelpers_test'
 
-        class TestColSubclass(PlainTextColumn):
+        class TestColSubclass(objecttable.PlainTextColumn):
             modelfield = 'testfield'
+
             def __init__(self, **kwargs):
-                super(TestColSubclass,self).__init__(**kwargs)
+                super(TestColSubclass, self).__init__(**kwargs)
 
         self.model_testobject = TestModel()
         view = mock.MagicMock()
@@ -74,13 +79,16 @@ class TestPlainTextColumn(TestCase):
 
     def test_render_cell(self):
         self.model_testobject.testfield = 'test_value'
-        self.assertEquals('test_value', 
-            self.column_subclass.render_cell(self.model_testobject).strip()) 
+        self.assertEquals(
+            '<span class="objecttable-cellvalue">test_value</span>',
+            self.column_subclass.render_cell(self.model_testobject).strip())
+
 
 class TestSingleActionColumn(TestCase):
     def setUp(self):
         class TestModel(models.Model):
             testfield = models.CharField(verbose_name=u'Test Value')
+
             class Meta:
                 app_label = 'django_cradmin.viewhelpers_test'
 
@@ -92,15 +100,17 @@ class TestSingleActionColumn(TestCase):
                 print 'running __str__!'
                 return self.testfield.verbose_name
 
-        class TestIncompleteColSubclass(SingleActionColumn):
+        class TestIncompleteColSubclass(objecttable.SingleActionColumn):
             modelfield = 'testfield'
-            def __init__(self, **kwargs):
-                super(TestIncompleteColSubclass,self).__init__(**kwargs)
 
-        class TestColSubclass(SingleActionColumn):
-            modelfield = 'testfield'
             def __init__(self, **kwargs):
-                super(TestColSubclass,self).__init__(**kwargs)
+                super(TestIncompleteColSubclass, self).__init__(**kwargs)
+
+        class TestColSubclass(objecttable.SingleActionColumn):
+            modelfield = 'testfield'
+
+            def __init__(self, **kwargs):
+                super(TestColSubclass, self).__init__(**kwargs)
 
             def get_actionurl(self, obj):
                 return 'www.example.com/{}'.format(obj.testfield)
@@ -120,7 +130,7 @@ class TestSingleActionColumn(TestCase):
             self.column_subclass_incomplete.render_cell(self.model_testobject)
 
     def test_render_cell(self):
-        expected = '<a href="www.example.com/test_value">test_value</a>'
+        expected = '<a href="www.example.com/test_value" class="objecttable-cellvalue-link">test_value</a>'
         self.assertEquals(self.column_subclass.render_cell(self.model_testobject), expected)
 
 
@@ -128,6 +138,7 @@ class TestMultiActionColumn(TestCase):
     def setUp(self):
         class TestModel(models.Model):
             testfield = models.CharField(verbose_name=u'Test Value')
+
             class Meta:
                 app_label = 'django_cradmin.viewhelpers_test'
 
@@ -139,26 +150,27 @@ class TestMultiActionColumn(TestCase):
                 print 'running __str__!'
                 return self.testfield.verbose_name
 
-        class TestIncompleteColSubclass(MultiActionColumn):
+        class TestIncompleteColSubclass(objecttable.MultiActionColumn):
             modelfield = 'testfield'
-            def __init__(self, **kwargs):
-                super(TestIncompleteColSubclass,self).__init__(**kwargs)
 
-        class TestColSubclass(MultiActionColumn):
-            modelfield = 'testfield'
             def __init__(self, **kwargs):
-                super(TestColSubclass,self).__init__(**kwargs)
+                super(TestIncompleteColSubclass, self).__init__(**kwargs)
+
+        class TestColSubclass(objecttable.MultiActionColumn):
+            modelfield = 'testfield'
+
+            def __init__(self, **kwargs):
+                super(TestColSubclass, self).__init__(**kwargs)
 
             def get_buttons(self, obj):
-                return [Button(label="Btn1", url="www.example.com/btn1"), 
-                        Button(label="Btn2", url="www.example.com/btn2")]
+                return [objecttable.Button(label="Btn1", url="www.example.com/btn1"),
+                        objecttable.Button(label="Btn2", url="www.example.com/btn2")]
 
         self.model_testobject = TestModel(testfield="test_value")
         view = mock.MagicMock()
         view.model = self.model_testobject
         self.column_subclass = TestColSubclass(view=view)
         self.column_subclass_incomplete = TestIncompleteColSubclass(view=view)
-
 
     def test_get_buttons_ExceptionRaised(self):
         with self.assertRaises(NotImplementedError):
@@ -169,31 +181,34 @@ class TestMultiActionColumn(TestCase):
             self.column_subclass_incomplete.render_cell(self.model_testobject)
 
     def test_render_cell(self):
-        expected = ['<p>test_value</p>','<p>',
-        '<a','href="www.example.com/btn1"','class="btn','btn-default', 'btn-sm">', 'Btn1', '</a>',
-        '<a','href="www.example.com/btn2"','class="btn','btn-default', 'btn-sm">', 'Btn2', '</a>',
-        '</p>']
-
+        expected = [
+            '<p', 'class="objecttable-cellvalue">test_value</p>',
+            '<p', 'class="objecttable-cellbuttons">',
+            '<a', 'href="www.example.com/btn1"', 'class="btn', 'btn-default', 'btn-sm">', 'Btn1', '</a>',
+            '<a', 'href="www.example.com/btn2"', 'class="btn', 'btn-default', 'btn-sm">', 'Btn2', '</a>',
+            '</p>'
+        ]
         result = self.column_subclass.render_cell(self.model_testobject).split()
         self.assertEquals(result, expected)
 
 
 class TestButton(TestCase):
     def test_render_with_icon_and_class(self):
-        btn = Button(label="My Btn", url="www.example.com/mybtnurl", buttonclass="danger", 
+        btn = objecttable.Button(
+            label="My Btn", url="www.example.com/mybtnurl",
+            buttonclass="danger",
             icon="glyphicon glyphicon-shopping-cart")
-
-        expected = ['<a', 'href="www.example.com/mybtnurl"', 'class="btn', 'btn-danger', 'btn-sm">',
-        '<i', 'class="glyphicon', 'glyphicon-shopping-cart"></i>', 'My', 'Btn', '</a>']
-
+        expected = [
+            '<a', 'href="www.example.com/mybtnurl"', 'class="btn', 'btn-danger', 'btn-sm">',
+            '<span', 'class="glyphicon', 'glyphicon-shopping-cart"></span>', 'My', 'Btn', '</a>'
+        ]
         result = btn.render().split()
         self.assertEquals(result, expected)
 
     def test_render_simple(self):
-        btn = Button(label="My Btn", url="www.example.com/mybtnurl")
-
-        expected = ['<a', 'href="www.example.com/mybtnurl"', 'class="btn', 'btn-default', 'btn-sm">',
-        'My', 'Btn', '</a>']
-
+        btn = objecttable.Button(label="My Btn", url="www.example.com/mybtnurl")
+        expected = [
+            '<a', 'href="www.example.com/mybtnurl"', 'class="btn', 'btn-default', 'btn-sm">',
+            'My', 'Btn', '</a>']
         result = btn.render().split()
         self.assertEquals(result, expected)
