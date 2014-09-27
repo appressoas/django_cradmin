164e807cf78f053efc793f20ca36d4aa9edff695
Espen A. Kristiansen <post@espenak.net>
2014-09-27 03:08:24 +0200

    Added cradmin_appurl template tag.

--- django_cradmin/templatetags/cradmin_tags.py
+++ django_cradmin/templatetags/cradmin_tags.py
@@ -26,3 +26,12 @@ def cradmin_rolefrontpage_url(context, role):
     request = context['request']
     cradmin_instance = cradmin_instance_registry.get_current_instance(request)
     return cradmin_instance.rolefrontpage_url(cradmin_instance.get_roleid(role))
+
+
+@register.simple_tag(takes_context=True)
+def cradmin_appurl(context, viewname, *args, **kwargs):
+    """
+    Template tag implementation of :meth:`django_cradmin.crapp.App.reverse_appurl`.
+    """
+    request = context['request']
+    return request.cradmin_app.reverse_appurl(viewname, args=args, kwargs=kwargs)
