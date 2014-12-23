da60b241cfd18a1414f21bae9336c0ec52c1048d
Espen A. Kristiansen <post@espenak.net>
2014-12-23 10:21:40 +0100

    templatetags.cradmin_image_tags: Added new cradmin_archiveimage_thumbnail tag.

--- django_cradmin/templatetags/cradmin_image_tags.py
+++ django_cradmin/templatetags/cradmin_image_tags.py
@@ -18,3 +18,21 @@ def cradmin_thumbnail(*args, **kwargs):
     except:
         logger.exception('cradmin_thumbnail failed. args=%r, kwargs=%r', args, kwargs)
         return ''
+
+
+@register.simple_tag
+def cradmin_archiveimage_thumbnail(archiveimage, **kwargs):
+    """
+    Template tag that creates a thumbnail for the given
+    :class:`django_cradmin.apps.cradmin_imagearchive.models.ArchiveImage`.
+
+    Parameters:
+        archiveimage: An ArchiveImage object.
+        kwargs: Same as for :meth:`django_cradmin.imageutils.request_thumbnail`.
+    """
+    image = archiveimage.image
+    try:
+        return imageutils.request_thumbnail(image, **kwargs).url
+    except:
+        logger.exception('cradmin_thumbnail failed. archiveimage=%r, kwargs=%r', archiveimage, kwargs)
+        return ''
