cad6fc2f1ba0ff3eaa89a98ba292b43b426af5b3
Espen Angell Kristiansen <post@espenak.net>
2015-01-05 12:07:22 +0100

    docs: Removed automatic API docs.

--- docs/fabfile.py
+++ docs/fabfile.py
@@ -9,15 +9,4 @@ def docs():
     """
     Build the docs.
     """
-    apidocdir = '_apidoc'
-    if os.path.exists(apidocdir):
-        rmtree(apidocdir)
-
-    exclude = glob('../django_cradmin/*/migrations/') \
-        + glob('../django_cradmin/*/tests/')
-    exclude = map(os.path.abspath, exclude)
-
-    local('sphinx-apidoc -o develop/_apidoc/ --no-toc ../django_cradmin {exclude}'.format(
-        exclude=' '.join(exclude)
-    ))
     local('sphinx-build -b html . _build')
