527dd0a435e3bc2c2cf1f3dacb32dfee64c1d9ba
Espen A. Kristiansen <post@espenak.net>
2015-01-24 20:09:53 +0100

    Configured media root.

--- django_cradmin_testsettings.py
+++ django_cradmin_testsettings.py
@@ -75,6 +75,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = 'test_django_media_root'
+
 # Django crispy forms:
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
