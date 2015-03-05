ea2094555f88a429be82c31ffb1f93f26741dd28
Espen A. Kristiansen <post@espenak.net>
2015-03-05 21:14:47 +0100

    cradmin_activate_account: Docs.

--- django_cradmin/apps/cradmin_activate_account/utils.py
+++ django_cradmin/apps/cradmin_activate_account/utils.py
@@ -8,11 +8,39 @@ from django_cradmin.apps.cradmin_generic_token_with_metadata.models import Gener
 
 
 class ActivationEmail(object):
+    """
+    Handles account activation. Sends an email with a link that
+    the user clicks to activate their account.
+
+    Example::
+
+        from django_cradmin.apps.cradmin_activate_account.utils import ActivationEmail
+
+        def myview(request):
+            someuser = # ... code to get a user object ...
+            ActivationEmail(request=request, user=someuser).send()
+    """
+
+    #: The email subject template.
     email_subject_template = 'cradmin_activate_account/email/subject.django.txt'
+
+    #: The email message template.
     email_message_template = 'cradmin_activate_account/email/message.django.txt'
+
+    #: The name of the app. Used for
+    #: :obj:`django_cradmin.apps.cradmin_generic_token_with_metadata.models.GenericTokenWithMetadata.app`.
+    #: If you override this, you also have to override :meth:`~.ActivationEmail.get_activate_url`
+    #: and return the URL to a view that pops a GenericTokenWithMetadata with the
+    #: changed appname.
     appname = 'cradmin_activate_account'
 
     def __init__(self, request, user, next_url=None):
+        """
+        Parameters:
+            request: A Django HttpRequest object.
+            user: The user you want to activate. Must have an ``email`` attribute or property.
+            next_url: An optional URL to redirect to after the user has activated their account.
+        """
         self.user = user
         self.request = request
         self.next_url = next_url or getattr(
@@ -25,6 +53,9 @@ class ActivationEmail(object):
         return self.email_message_template
 
     def get_activate_url(self, token):
+        """
+        Get the activate account view URL.
+        """
         return reverse('cradmin-activate-account-activate', kwargs={
             'token': token.token
         })
@@ -42,6 +73,9 @@ class ActivationEmail(object):
         ).strip()
 
     def get_email_template_context_data(self, token):
+        """
+        Get the context data of the email templates.
+        """
         return {
             'user': self.user,
             'DJANGO_CRADMIN_SITENAME': settings.DJANGO_CRADMIN_SITENAME,
@@ -49,9 +83,19 @@ class ActivationEmail(object):
         }
 
     def get_from_email(self):
+        """
+        Get the email sender address.
+
+        Defaults to the ``DJANGO_CRADMIN_ACTIVATE_ACCOUNT_FROM_EMAIL`` setting
+        falling back on the ``DEFAULT_FROM_EMAIL`` setting.
+        """
         return getattr(settings, 'DJANGO_CRADMIN_ACTIVATE_ACCOUNT_FROM_EMAIL', settings.DEFAULT_FROM_EMAIL)
 
     def get_expiration_datetime(self):
+        """
+        Get the value to use for the ``expiration_datetime`` attribute of
+        :class:`~django_cradmin.apps.cradmin_generic_token_with_metadata.models.GenericTokenWithMetadata`.
+        """
         return get_expiration_datetime_for_app(self.appname)
 
     def generate_token(self):
--- docs/apps.cradmin_activate_account.rst
+++ docs/apps.cradmin_activate_account.rst
@@ -33,6 +33,19 @@ And add something like this to your root url config::
 
 
 
+***********
+Quick start
+***********
+If you install and setup the app as explained above, you just need to set
+the ``DJANGO_CRADMIN_SITENAME`` setting::
+
+    DJANGO_CRADMIN_SITENAME = 'Testsite'
+
+Now you should be able to use the
+:class:`~django_cradmin.apps.cradmin_activate_account.utils.ActivationEmail`
+class to initiate account activation.
+
+
 *********
 Configure
 *********
@@ -42,7 +55,6 @@ Required settings:
         The name of the site.
         You **must set this setting** unless you override the email subject
         and message templates as explained in :ref:`activate_account_emailtemplates`.
-
 
 
 Optional settings:
@@ -60,11 +72,17 @@ How it works
 
 Step One --- Send the activate account email
 ============================================
-TODO
+First we generate a token using :doc:`apps.cradmin_generic_token_with_metadata`.
+This is a single use token with the user and the *next url* as metadata.
+We send an email with a link containing this token.
+
 
 Step two --- Activate the account
 =================================
-TODO
+When the user clicks on the email, we get the user and *next url* from the
+token (which is part of the URL). If the token validates, we activate
+the account, add a success message using the Django message framework,
+and redirect to the *next url*.
 
 
 .. _activate_account_emailtemplates:
@@ -95,3 +113,12 @@ All of the email templates get the following context variables:
 - ``DJANGO_CRADMIN_SITENAME``: The value of the setting with the same name.
 - ``activate_url``: The URL that users should click to activate their account.
 - ``user``: The user that is activating their account.
+
+
+
+*************************
+The ActivationEmail class
+*************************
+
+.. autoclass:: django_cradmin.apps.cradmin_activate_account.utils.ActivationEmail
+    :members:
