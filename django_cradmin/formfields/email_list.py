3ad5810a51cbb7bc8889ceb759f95373f257b6d4
Espen A. Kristiansen <post@espenak.net>
2015-03-07 22:19:12 +0100

    formfields: Added an EmailList form field.

---
+++ django_cradmin/formfields/email_list.py
@@ -0,0 +1,39 @@
+import re
+from django import forms
+from django.core import validators
+from django.core.validators import EmailValidator
+from django.utils.translation import ugettext_lazy as _
+
+
+class EmailListField(forms.Field):
+    default_error_messages = {
+        'invalid': _('Type email-addresses separated by newlines, whitespace or comma.')
+    }
+    stringsplit_regex = re.compile(r'(?:\s*,\s*)|\s+')
+    email_validator = EmailValidator()
+
+    def string_to_list(self, value):
+        return self.stringsplit_regex.split(value)
+
+    def to_python_list(self, value):
+        if value in validators.EMPTY_VALUES:
+            return []
+        elif isinstance(value, list):
+            return value
+        elif isinstance(value, tuple):
+            return list(value)
+        else:
+            return self.string_to_list(value)
+
+    def validate_list_of_emails(self, list_of_emails):
+        for email in list_of_emails:
+            try:
+                self.email_validator(email)
+            except forms.ValidationError:
+                errormessage = _('Invalid email address: %(email)s') % {'email': email}
+                raise forms.ValidationError(errormessage)
+
+    def to_python(self, value):
+        list_of_emails = self.to_python_list(value)
+        self.validate_list_of_emails(list_of_emails)
+        return list_of_emails
