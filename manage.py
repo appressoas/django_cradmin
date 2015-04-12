a5832f1230f57f7b755ceeacea29cbfb74275314
Espen A. Kristiansen <post@espenak.net>
2015-04-12 19:42:57 +0200

    manage.py: Added (commented out) warning debugging code.

--- manage.py
+++ manage.py
@@ -6,6 +6,11 @@ Django management files for running the django_cradmin tests.
 Does NOT work as a full project (no database, no urls, etc.)
 """
 
+# import warnings
+# from django.utils.deprecation import RemovedInDjango19Warning
+# warnings.filterwarnings('ignore', message='Not importing directory .*')
+# warnings.filterwarnings('ignore', category=RemovedInDjango19Warning)
+
 
 import os
 import sys
