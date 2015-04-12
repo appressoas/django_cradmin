7b09b3f93cef69d26853753922985baf8812f2c8
Espen A. Kristiansen <post@espenak.net>
2015-04-13 01:50:48 +0200

    django_cradmin.tests.helpers: Updated create_image to use BytesIO instead of StringIO.

--- django_cradmin/apps/cradmin_imagearchive/tests/helpers.py
+++ django_cradmin/apps/cradmin_imagearchive/tests/helpers.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from future import standard_library
 standard_library.install_aliases()
 from PIL import Image, ImageDraw
-from io import StringIO
+from io import BytesIO
 
 
 def create_image(width, height, filetype='PNG'):
@@ -14,6 +14,6 @@ def create_image(width, height, filetype='PNG'):
     """
     im = Image.new('RGB', (width, height), color='#191919')
     ImageDraw.Draw(im)
-    out = StringIO()
+    out = BytesIO()
     im.save(out, filetype)
     return out.getvalue()
