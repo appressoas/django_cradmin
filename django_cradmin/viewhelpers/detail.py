a6f953c61ce3e07752f2afbea0ec4d4935b8db4c
Espen A. Kristiansen <post@espenak.net>
2015-06-08 18:28:46 +0200

    viewhelpers.detail: Added DetailRoleView.

--- django_cradmin/viewhelpers/detail.py
+++ django_cradmin/viewhelpers/detail.py
@@ -7,3 +7,19 @@ class DetailView(QuerysetForRoleMixin, DjangoDetailView):
     """
     Base detail view for Django cradmin views.
     """
+
+
+class DetailRoleView(DetailView):
+    """
+    Extends :class:`.DetailView` to streamline creating a detail view
+    for the current role object.
+
+    Just like :class:`.DetailView`, but with the get_object and
+    get_queryset_for_role methods implemented to edit the current role
+    object.
+    """
+    def get_object(self, queryset=None):
+        return self.get_queryset_for_role(self.request.cradmin_role).get()
+
+    def get_queryset_for_role(self, role):
+        return self.model.objects.filter(pk=role.pk)
