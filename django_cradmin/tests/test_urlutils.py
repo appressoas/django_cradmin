f37796cf8b660804b8bfb00f50cce792cf9e5fc3
Espen A. Kristiansen <post@espenak.net>
2015-08-06 16:15:37 +0200

    urlutils: Ignore None by default.

--- django_cradmin/tests/test_urlutils.py
+++ django_cradmin/tests/test_urlutils.py
@@ -12,23 +12,39 @@ from urllib.parse import urlsplit
 class TestUpdateQuerystring(TestCase):
     def test_simple(self):
         self.assertEqual(
-            urlutils.update_querystring('http://example.com', next='http://example.com/admin'),
+            urlutils.update_querystring('http://example.com',
+                                        {'next': 'http://example.com/admin'}),
             'http://example.com?next=http%3A%2F%2Fexample.com%2Fadmin'
         )
 
     def test_update_existing(self):
         self.assertEqual(
             urlutils.update_querystring('http://example.com?next=/test',
-                                        next='/updated'),
+                                        {'next': '/updated'}),
             'http://example.com?next=%2Fupdated'
         )
 
     def test_keep_existing(self):
         url = urlutils.update_querystring('http://example.com?next=/test',
-                                          other='otherstuff')
+                                          {'other': 'otherstuff'})
         url_split = urlsplit(url)
         querydict = QueryDict(url_split.query)
         self.assertEqual(querydict.dict(), {
             'next': '/test',
             'other': 'otherstuff',
         })
+
+    def test_ignore_none_values(self):
+        self.assertEqual(
+            urlutils.update_querystring('http://example.com',
+                                        {'next': None}),
+            'http://example.com'
+        )
+
+    def test_do_not_ignore_none_values(self):
+        self.assertEqual(
+            urlutils.update_querystring('http://example.com',
+                                        {'next': None}, ignore_none_values=False),
+            'http://example.com?next=None'
+        )
+
--- django_cradmin/urlutils.py
+++ django_cradmin/urlutils.py
@@ -5,12 +5,14 @@ install_aliases()
 from urllib.parse import urlsplit, urlunsplit
 
 
-def update_querystring(url, **querystringargs):
+def update_querystring(url, querystringargs, ignore_none_values=True):
     """
     Update the querystring portion of the given ``url``.
 
     Parameters:
-        querystringargs: The querystring args to add/replace.
+        querystringargs (dict): The querystring args to add/replace.
+        ignore_none_values (bool): If this is ``True`` (default),
+            we ignore ``None`` values in ``querystringargs``.
 
     Returns:
         The updated url.
@@ -20,16 +22,18 @@ def update_querystring(url, **querystringargs):
         Add querystring argument::
 
             from django_cradmin import urlutils
-            urlutils.update_querystring('http://example.com', search='test')
+            urlutils.update_querystring('http://example.com', {'search': 'test'})
 
         Update querystring argument::
 
             urlutils.update_querystring('http://example.com?search=something&page=2',
-                search='updated')
+                {'search': 'updated'})
     """
     parsed_url = urlsplit(url)
     querydict = QueryDict(parsed_url.query, mutable=True)
     for key, value in querystringargs.items():
+        if ignore_none_values and value is None:
+            continue
         querydict[key] = value
     return urlunsplit((
         parsed_url.scheme,
