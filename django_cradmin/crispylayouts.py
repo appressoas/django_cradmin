dca2e94fbcc9d4ec2d1b5cd4539fdd3dc7b5ce89
Espen A. Kristiansen <post@espenak.net>
2015-08-11 04:13:32 +0200

    Added support for a collapsible section in crispy layouts.

--- django_cradmin/crispylayouts.py
+++ django_cradmin/crispylayouts.py
@@ -2,12 +2,15 @@
 Custom django-crispy-forms layouts.
 """
 from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
+from crispy_forms import layout
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 from crispy_forms.utils import flatatt
+from django.template.loader import render_to_string
+from django_cradmin.templatetags.cradmin_icon_tags import cradmin_icon
 
 
-class CradminSubmitButton(Submit):
+class CradminSubmitButton(layout.Submit):
     template = 'django_cradmin/crispylayouts/submitbutton.django.html'
     extra_button_attributes = {}
 
@@ -34,6 +37,33 @@ class DangerSubmit(CradminSubmitButton):
 
 class DefaultSubmit(CradminSubmitButton):
     field_classes = "btn btn-default"
+
+
+class CollapsedSectionLayout(layout.Div):
+    wrapper_template_name = 'django_cradmin/crispylayouts/collapsed-section-layout.django.html'
+
+    def __init__(self, *args, **kwargs):
+        self.show_label = kwargs.pop('show_label', _('Show'))
+        self.hide_label = kwargs.pop('hide_label', _('Hide'))
+        self.show_icon = kwargs.pop('show_icon', cradmin_icon('caret-down'))
+        self.hide_icon = kwargs.pop('hide_icon', cradmin_icon('caret-up'))
+        super(CollapsedSectionLayout, self).__init__(*args, **kwargs)
+
+    def get_wrapper_context_data(self):
+        return {
+            'show_label': self.show_label,
+            'hide_label': self.hide_label,
+            'show_icon': self.show_icon,
+            'hide_icon': self.hide_icon,
+        }
+
+    def render(self, *args, **kwargs):
+        content = super(CollapsedSectionLayout, self).render(*args, **kwargs)
+        context = {
+            'content': content
+        }
+        context.update(self.get_wrapper_context_data())
+        return render_to_string(self.wrapper_template_name, context)
 
 
 class CradminFormHelper(FormHelper):
