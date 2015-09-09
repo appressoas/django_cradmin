c86064f8903c33ee669f97cf07b8446d92451546
Espen A. Kristiansen <post@espenak.net>
2015-09-09 16:26:06 +0200

    Made the setfieldvalue directive support moving focus after applying a value.

--- django_cradmin/static/django_cradmin/dist/js/cradmin.js
+++ django_cradmin/static/django_cradmin/dist/js/cradmin.js
@@ -2806,6 +2806,22 @@
         Set value to 2015-12-24 12:30
     </button>
   ```
+  
+  You can make the directive change the focus on click after applying the
+  value with ``django-cradmin-setfieldvalue-move-focus-on-click="<id>"``:
+  
+  ```
+    <button type="button"
+            django-cradmin-setfieldvalue="2015-12-24 12:30"
+            django-cradmin-setfieldvalue-field-id="my_datetimefield_id"
+            django-cradmin-setfieldvalue-move-focus-on-click="my_datetimefield_id">
+        Set value to 2015-12-24 12:30
+    </button>
+  ```
+  
+  
+  Can also be used on ``<a>``-elements. The directive uses ``e.preventDefault``
+  to ensure the href is not triggered.
   */
 
 
@@ -2814,17 +2830,25 @@
       return {
         scope: {
           value: "@djangoCradminSetfieldvalue",
-          fieldid: "@djangoCradminSetfieldvalueFieldId"
+          fieldid: "@djangoCradminSetfieldvalueFieldId",
+          moveFocusOnClick: "@djangoCradminSetfieldvalueMoveFocusOnClick"
         },
         link: function($scope, $element) {
-          var fieldElement;
+          var fieldElement, focusElement;
           fieldElement = angular.element("#" + $scope.fieldid);
+          if ($scope.moveFocusOnClick != null) {
+            focusElement = angular.element("#" + $scope.moveFocusOnClick);
+          }
           if (fieldElement.length === 0) {
             return typeof console !== "undefined" && console !== null ? typeof console.error === "function" ? console.error("Could not find a field with the '" + $scope.fieldid + "' ID.") : void 0 : void 0;
           } else {
-            $element.on('click', function() {
+            $element.on('click', function(e) {
+              e.preventDefault();
               fieldElement.val($scope.value);
-              return fieldElement.trigger('change');
+              fieldElement.trigger('change');
+              if (focusElement != null) {
+                return focusElement.focus();
+              }
             });
           }
         }
--- django_cradmin/static/django_cradmin/src/lib/forms/setfieldvalue.coffee
+++ django_cradmin/static/django_cradmin/src/lib/forms/setfieldvalue.coffee
@@ -12,6 +12,22 @@ Example:
       Set value to 2015-12-24 12:30
   </button>
 ```
+
+You can make the directive change the focus on click after applying the
+value with ``django-cradmin-setfieldvalue-move-focus-on-click="<id>"``:
+
+```
+  <button type="button"
+          django-cradmin-setfieldvalue="2015-12-24 12:30"
+          django-cradmin-setfieldvalue-field-id="my_datetimefield_id"
+          django-cradmin-setfieldvalue-move-focus-on-click="my_datetimefield_id">
+      Set value to 2015-12-24 12:30
+  </button>
+```
+
+
+Can also be used on ``<a>``-elements. The directive uses ``e.preventDefault``
+to ensure the href is not triggered.
 ###
 app.directive 'djangoCradminSetfieldvalue', [
   ->
@@ -19,16 +35,22 @@ app.directive 'djangoCradminSetfieldvalue', [
       scope: {
         value: "@djangoCradminSetfieldvalue"
         fieldid: "@djangoCradminSetfieldvalueFieldId"
+        moveFocusOnClick: "@djangoCradminSetfieldvalueMoveFocusOnClick"
       }
 
       link: ($scope, $element) ->
         fieldElement = angular.element("##{$scope.fieldid}")
+        if $scope.moveFocusOnClick?
+          focusElement = angular.element("##{$scope.moveFocusOnClick}")
         if fieldElement.length == 0
           console?.error? "Could not find a field with the '#{$scope.fieldid}' ID."
         else
-          $element.on 'click', ->
+          $element.on 'click', (e) ->
+            e.preventDefault()
             fieldElement.val $scope.value
             fieldElement.trigger 'change'
+            if focusElement?
+              focusElement.focus()
           return
     }
 ]
