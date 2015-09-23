8723cc3d1e9e73bcc55602492021d5d2e1861aac
Espen A. Kristiansen <post@espenak.net>
2015-09-23 18:32:05 +0200

    Cleanup old stuff from cradmin_image_tags.

--- django_cradmin/templatetags/cradmin_image_tags.py
+++ django_cradmin/templatetags/cradmin_image_tags.py
@@ -8,36 +8,6 @@ register = Library()
 logger = logging.getLogger(__name__)
 
 
-# @register.simple_tag
-# def cradmin_thumbnail(*args, **kwargs):
-#     """
-#     Template tag that forwards all arguments to
-#     :meth:`django_cradmin.imageutils.request_thumbnail`.
-#     """
-#     try:
-#         return imageutils.request_thumbnail(*args, **kwargs).url
-#     except:
-#         logger.exception('cradmin_thumbnail failed. args=%r, kwargs=%r', args, kwargs)
-#         return ''
-#
-#
-# @register.simple_tag
-# def cradmin_archiveimage_thumbnail(archiveimage, **kwargs):
-#     """
-#     Template tag that creates a thumbnail for the given
-#     :class:`django_cradmin.apps.cradmin_imagearchive.models.ArchiveImage`.
-#
-#     Parameters:
-#         archiveimage: An ArchiveImage object.
-#         kwargs: Same as for :meth:`django_cradmin.imageutils.request_thumbnail`.
-#     """
-#     image = archiveimage.image
-#     try:
-#         return imageutils.request_thumbnail(image, **kwargs).url
-#     except:
-#         logger.exception('cradmin_thumbnail failed. archiveimage=%r, kwargs=%r', archiveimage, kwargs)
-#         return ''
-
 @register.simple_tag(takes_context=True)
 def cradmin_transform_image(context, imageurl, **options):
     """
