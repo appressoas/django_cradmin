b2cfba20964cc76345e95c153eeec2c748c96b2b
Espen A. Kristiansen <post@espenak.net>
2015-09-24 00:13:29 +0200

    imagearchive: Better admin UI. Include file size and link to the image in the listing.

--- django_cradmin/apps/cradmin_imagearchive/admin.py
+++ django_cradmin/apps/cradmin_imagearchive/admin.py
@@ -1,19 +1,24 @@
 from __future__ import unicode_literals
 from django.contrib import admin
+from django.template import defaultfilters
 
 from django_cradmin.apps.cradmin_imagearchive.models import ArchiveImage
 
 
 class ArchiveImageAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'name',
         'description',
         'role',
         'image_width',
         'image_height',
+        'get_size',
+        'get_url',
     )
 
     search_fields = [
+        'id',
         'name',
         'description',
     ]
@@ -29,5 +34,19 @@ class ArchiveImageAdmin(admin.ModelAdmin):
             return super(ArchiveImageAdmin, self).get_queryset(request)
         else:
             return ArchiveImage.objects.none()
+
+    def get_size(self, obj):
+        return defaultfilters.filesizeformat(obj.image.size)
+    get_size.short_description = 'Size'
+
+    def get_url(self, obj):
+        return '<a href="{url}" target="_blank">{label}</a>'.format(
+            label='Show image',
+            url=obj.image.url
+        )
+    get_url.short_description = 'Open'
+    get_url.allow_tags = True
+
+
 
 admin.site.register(ArchiveImage, ArchiveImageAdmin)
