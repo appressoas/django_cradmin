c50b3a5d8001fe3abe630818710dc4e56a3d0d61
Espen A. Kristiansen <post@espenak.net>
2015-10-17 19:42:09 +0200

    utils.crhumanize: Added dehumanize_readable_filesize and docs.

--- django_cradmin/tests/utils/test_crhumanize.py
+++ django_cradmin/tests/utils/test_crhumanize.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django_cradmin.utils import crhumanize
 
 
-class TestCrhumanize(TestCase):
+class TestHumanReadableFileSize(TestCase):
     def test_less_than_kb(self):
         self.assertEqual('0B', crhumanize.human_readable_filesize(0))
         self.assertEqual('999B', crhumanize.human_readable_filesize(999))
@@ -22,3 +22,25 @@ class TestCrhumanize(TestCase):
     def test_tb(self):
         self.assertEqual('1.00TB', crhumanize.human_readable_filesize(1000000000000))
         self.assertEqual('55.56TB', crhumanize.human_readable_filesize(55555555555555))
+
+
+class TestDehumanizeReadableFileSize(TestCase):
+    def test_less_than_kb(self):
+        self.assertEqual(0, crhumanize.dehumanize_readable_filesize('0B'))
+        self.assertEqual(999, crhumanize.dehumanize_readable_filesize('999B'))
+
+    def test_kb(self):
+        self.assertEqual(1000, crhumanize.dehumanize_readable_filesize('1KB'))
+        self.assertEqual(999000, crhumanize.dehumanize_readable_filesize('999KB'))
+
+    def test_mb(self):
+        self.assertEqual(1000000, crhumanize.dehumanize_readable_filesize('1.0MB'))
+        self.assertEqual(55600000, crhumanize.dehumanize_readable_filesize('55.6MB'))
+
+    def test_gb(self):
+        self.assertEqual(1000000000, crhumanize.dehumanize_readable_filesize('1.00GB'))
+        self.assertEqual(55560000000, crhumanize.dehumanize_readable_filesize('55.56GB'))
+
+    def test_tb(self):
+        self.assertEqual(1000000000000, crhumanize.dehumanize_readable_filesize('1.00TB'))
+        self.assertEqual(55560000000000, crhumanize.dehumanize_readable_filesize('55.56TB'))
--- django_cradmin/utils/crhumanize.py
+++ django_cradmin/utils/crhumanize.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+from builtins import str
 
 
 def human_readable_filesize(size_in_bytes):
@@ -6,6 +8,16 @@ def human_readable_filesize(size_in_bytes):
 
     Returns a number suffixed with ``B``, ``KB``, ``MB``,
     ``GB`` or ``TB``.
+
+    Examples:
+
+        >>> from django_cradmin.utils import crhumanize
+        >>> crhumanize.human_readable_filesize(1)
+        '1B'
+        >>> crhumanize.human_readable_filesize(2344234345)
+        '2.34GB'
+        >>> crhumanize.human_readable_filesize(23442343451234)
+        '23.44TB'
     """
     if size_in_bytes < 1000:
         return '{}B'.format(size_in_bytes)
@@ -17,3 +29,37 @@ def human_readable_filesize(size_in_bytes):
         return '{:.2f}GB'.format(size_in_bytes / 1000000000.0)
     else:
         return '{:.2f}TB'.format(size_in_bytes / 1000000000000.0)
+
+
+def dehumanize_readable_filesize(humanized_size):
+    """
+    Does the opposite of :func:`.human_readable_filesize`.
+
+    Takes a string containing a number suffixed with ``B``, ``KB``, ``MB``,
+    ``GB`` or ``TB``, and returns an int with the number of bytes.
+
+    Examples:
+
+        >>> from django_cradmin.utils import crhumanize
+        >>> crhumanize.dehumanize_readable_filesize('999B')
+        999
+        >>> crhumanize.dehumanize_readable_filesize('2.34GB')
+        2340000000
+        >>> crhumanize.dehumanize_readable_filesize('43.312TB')
+        43312000000000
+    """
+    if not isinstance(humanized_size, str):
+        raise ValueError('humanized_size must be a string (unicode in python 2)')
+    humanized_size = humanized_size[:-1]
+    if humanized_size[-1].isdigit():
+        return int(humanized_size)
+    else:
+        sizechar = humanized_size[-1]
+        number = float(humanized_size[:-1])
+        multiply_by = {
+            'K': 1000,
+            'M': 1000000,
+            'G': 1000000000,
+            'T': 1000000000000
+        }[sizechar]
+        return int(number * multiply_by)
