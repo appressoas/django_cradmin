f88477629650e20c936d2204f093c93ed2d74e09
Espen A. Kristiansen <post@espenak.net>
2015-10-31 15:46:23 +0100

    theme: Basic floatgridlist styling.

--- django_cradmin/viewhelpers/listbuilder/list.py
+++ django_cradmin/viewhelpers/listbuilder/list.py
@@ -9,7 +9,11 @@ class RowList(base.List):
 
 
 class FloatGridList(base.List):
+
+    def get_css_size_class(self):
+        return 'django-cradmin-listbuilder-floatgridlist-md'
+
     def get_css_classes(self):
         css_classes = super(FloatGridList, self).get_css_classes()
-        css_classes += ' django-cradmin-listbuilder-floatgridlist'
+        css_classes += ' django-cradmin-listbuilder-floatgridlist ' + self.get_css_size_class()
         return css_classes
