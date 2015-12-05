5ec6873c85f3321f1fac70c5ad89b7650705b8e4
Espen A. Kristiansen <post@espenak.net>
2015-12-05 04:59:02 +0100

    viewhelpers.listfilter: Started working on a search filter.

--- django_cradmin/viewhelpers/listfilter/django/single/input.py
+++ django_cradmin/viewhelpers/listfilter/django/single/input.py
@@ -1,8 +1,40 @@
-from .base import AbstractDjangoOrmSingleFilter
+from django.utils.translation import ugettext_lazy
+from django_cradmin.viewhelpers.listfilter.django.base import AbstractDjangoOrmFilter
 
 
-class AbstractInputFilter(AbstractDjangoOrmSingleFilter):
+class AbstractInputFilter(AbstractDjangoOrmFilter):
     """
     Abstract base class for any filter that uses a single text input field.
     """
     template_name = 'django_cradmin/viewhelpers/listfilter/django/single/input/base.django.html'
+
+    def get_input_html_element_type(self):
+        """
+        Get the ``type``-attribute of the input HTML element.
+
+        Defaults to ``"text"``.
+        """
+        return 'text'
+
+    def get_base_css_classes_list(self):
+        css_classes = super(AbstractInputFilter, self).get_base_css_classes_list()
+        css_classes.append('django-cradmin-listfilter-filter-single-input')
+        return css_classes
+
+    def get_placeholder(self):
+        """
+        Get the placeholder text for the input element.
+
+        Defaults to empty string.
+        """
+        return ''
+
+
+class Search(AbstractInputFilter):
+    def get_base_css_classes_list(self):
+        css_classes = super(Search, self).get_base_css_classes_list()
+        css_classes.append('django-cradmin-listfilter-filter-single-input-search')
+        return css_classes
+
+    def get_placeholder(self):
+        return ugettext_lazy('Search ...')
