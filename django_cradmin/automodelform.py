f281dfb744f7fac89ce3638072bf210f3cc67385
Espen A. Kristiansen <post@espenak.net>
2015-12-18 10:18:45 +0100

    automodelform: Handle datefield.

--- django_cradmin/automodelform.py
+++ django_cradmin/automodelform.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from django_cradmin.widgets.datetimepicker import DateTimePickerWidget
+from django_cradmin.widgets import datetimepicker
 
 
 class ModelForm(forms.ModelForm):
@@ -9,17 +9,19 @@ class ModelForm(forms.ModelForm):
         self.autosetup_fields()
 
     def setup_datetime_field(self, fieldname, formfield):
-        self.fields[fieldname].widget = DateTimePickerWidget(
+        self.fields[fieldname].widget = datetimepicker.DateTimePickerWidget(
             required=formfield.required
         )
 
-    def setup_datetime_field(self, fieldname, formfield):
-        self.fields[fieldname].widget = DateTimePickerWidget(
+    def setup_date_field(self, fieldname, formfield):
+        self.fields[fieldname].widget = datetimepicker.DatePickerWidget(
             required=formfield.required
         )
 
     def setup_field(self, fieldname, formfield):
-        if isinstance(formfield, forms.DateTimeField):
+        if isinstance(formfield, forms.DateField):
+            self.setup_date_field(fieldname=fieldname, formfield=formfield)
+        elif isinstance(formfield, forms.DateTimeField):
             self.setup_datetime_field(fieldname=fieldname, formfield=formfield)
 
     def autosetup_fields(self):
