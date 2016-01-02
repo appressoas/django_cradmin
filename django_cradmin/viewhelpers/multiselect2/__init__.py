775c28c0f7b37ab374a73ff792b873cca7a9dd2f
Espen A. Kristiansen <post@espenak.net>
2016-01-03 00:13:11 +0100

    multiselect2: Include selected_item_renderer in __init__.

--- django_cradmin/viewhelpers/multiselect2/__init__.py
+++ django_cradmin/viewhelpers/multiselect2/__init__.py
@@ -2,3 +2,4 @@ from . import listbuilder_itemvalues  # noqa
 from . import target_renderer  # noqa
 from . import manytomanyview  # noqa
 from . import manytomanywidget  # noqa
+from . import selected_item_renderer  # noqa
