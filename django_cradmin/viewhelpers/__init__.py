1886252eacdcbb916eba9696c939e961826ce0f9
Espen A. Kristiansen <post@espenak.net>
2016-07-04 10:12:28 +0200

    viewhelpers: Made it importable as "from django_cradmin import viewhelpers".

---
+++ django_cradmin/viewhelpers/__init__.py
@@ -0,0 +1,23 @@
+from . import mixins  # noqa
+from . import crudbase  # noqa
+from . import create  # noqa
+from . import update  # noqa
+from . import delete  # noqa
+from . import detail  # noqa
+from . import formbase  # noqa
+from . import listbuilderview  # noqa
+from . import multiselect  # noqa
+from . import objecttable  # noqa
+
+__all__ = [
+    'mixins',
+    'crudbase',
+    'create',
+    'update',
+    'delete',
+    'detail',
+    'formbase',
+    'listbuilderview',
+    'multiselect',
+    'objecttable',
+]
