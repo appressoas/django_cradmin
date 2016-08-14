1f47d5771534a671cc00259ef30784eae3310f92
Espen A. Kristiansen <post@espenak.net>
2016-08-14 17:50:30 +0200

    views.roleselect: Updated for the 2.0 theme.
    See #58.

--- django_cradmin/views/roleselect.py
+++ django_cradmin/views/roleselect.py
@@ -12,7 +12,6 @@ class RoleSelectView(ListView):
     context_object_name = 'roles'
     pagetitle = _('What would you like to edit?')
     autoredirect_if_single_role = True
-    list_cssclass = 'django-cradmin-roleselect-list django-cradmin-roleselect-list-flat'
 
     def get_queryset(self):
         cradmin_instance = cradmin_instance_registry.get_current_instance(self.request)
@@ -37,11 +36,7 @@ class RoleSelectView(ListView):
     def get_pagetitle(self):
         return self.pagetitle
 
-    def get_list_cssclass(self):
-        return self.list_cssclass
-
     def get_context_data(self, **kwargs):
         context = super(RoleSelectView, self).get_context_data(**kwargs)
         context['pagetitle'] = self.get_pagetitle()
-        context['list_cssclass'] = self.get_list_cssclass()
         return context
