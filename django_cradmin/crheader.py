393f68ee7f870464726d1361272173ac725ba4a6
Espen A. Kristiansen <post@espenak.net>
2016-09-05 11:38:21 +0200

    theme_default: Start work on page-header and page-cover.

--- django_cradmin/crheader.py
+++ django_cradmin/crheader.py
@@ -14,11 +14,11 @@ class AbstractHeaderRenderable(renderable.AbstractRenderableWithCss):
         return 'header'
 
     def get_wrapper_htmltag_id(self):
-        return 'id_django_cradmin_header'
-
-    def get_base_css_classes_list(self):
-        return ['page-header']
+        return 'id_django_cradmin_page_header'
 
 
 class DefaultHeaderRenderable(AbstractHeaderRenderable):
     template_name = 'django_cradmin/crheader/default-header.django.html'
+
+    def get_base_css_classes_list(self):
+        return ['page-header', 'page-header--light']
