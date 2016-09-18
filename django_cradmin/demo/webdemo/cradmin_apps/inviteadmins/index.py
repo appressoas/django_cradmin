0139686472f980284587b47da0a8afe70e5d53a9
Espen A. Kristiansen <post@espenak.net>
2016-09-18 10:43:27 +0200

    webdemo: Cleanup.

--- django_cradmin/demo/webdemo/cradmin_apps/inviteadmins/index.py
+++ django_cradmin/demo/webdemo/cradmin_apps/inviteadmins/index.py
@@ -5,38 +5,6 @@ from django_cradmin.viewhelpers import listbuilderview
 from .mixins import QuerysetForRoleMixin
 
 
-# class EmailColumn(objecttable.MultiActionColumn):
-#     modelfield = 'id'
-#
-#     def get_header(self):
-#         return _('Email')
-#
-#     def get_buttons(self, obj):
-#         return [
-#             objecttable.Button(
-#                 label=_('Delete'),
-#                 url=self.reverse_appurl('delete', args=[obj.id]),
-#                 buttonclass="btn btn-danger btn-sm"),
-#         ]
-#
-#     def render_value(self, obj):
-#         return obj.metadata['email']
-#
-#     def is_sortable(self):
-#         return False
-#
-#
-# class CreatedDatetimeColumn(objecttable.DatetimeColumn):
-#     modelfield = 'created_datetime'
-#
-#     def get_default_ordering(self):
-#         return None
-#
-#
-# class ExpirationDatetimeColumn(objecttable.DatetimeColumn):
-#     modelfield = 'expiration_datetime'
-
-
 class InviteItemValue(listbuilder.itemvalue.TitleDescription):
     template_name = 'webdemo/inviteadmins/generic_token_with_metadata_itemvalue.django.html'
     valuealias = 'token'
@@ -59,11 +27,3 @@ class Overview(QuerysetForRoleMixin,
 
     def get_pagetitle(self):
         return 'Invite admins'
-
-    # def get_buttons(self):
-    #     app = self.request.cradmin_app
-    #     return [
-    #         objecttable.Button(label=_('Send private invite'),
-    #                            url=app.reverse_appurl('send'),
-    #                            buttonclass='btn btn-primary'),
-    #     ]
