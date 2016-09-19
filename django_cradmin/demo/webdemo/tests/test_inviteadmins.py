818227f0bf31e8933eaf9e65cc4ca45a7b8f932f
Espen A. Kristiansen <post@espenak.net>
2016-09-19 04:18:12 +0200

    webdemo...test_invite_admins: Use cradmin_testhelpers.

--- django_cradmin/demo/webdemo/tests/test_inviteadmins.py
+++ django_cradmin/demo/webdemo/tests/test_inviteadmins.py
@@ -1,75 +1,53 @@
 from __future__ import unicode_literals
-from django.test import TestCase, RequestFactory
+
 from django.core import mail
-import htmls
-from django_cradmin.python2_compatibility import mock
-from django_cradmin.demo.webdemo.cradmin_apps.inviteadmins.send import SendInvitesView
-
-from django_cradmin.demo.webdemo.models import Site
+from django.test import TestCase
+from django_cradmin import cradmin_testhelpers
 from django_cradmin.apps.cradmin_generic_token_with_metadata.models import GenericTokenWithMetadata
+from django_cradmin.demo.webdemo.cradmin_apps import inviteadmins
+from model_mommy import mommy
 
 
-class TestInviteAdmins(TestCase):
-    def setUp(self):
-        self.testsite = Site.objects.create(
-            name='testsite')
-        self.factory = RequestFactory()
+class TestInviteAdmins(TestCase, cradmin_testhelpers.TestCaseMixin):
+    viewclass = inviteadmins.send.SendInvitesView
 
     def test_post_invalid_email_no_invite_sent(self):
-        request = self.factory.post('/test', {
-            'emails': "invalid"
-        })
-        request.cradmin_role = self.testsite
         self.assertEqual(GenericTokenWithMetadata.objects.count(), 0)
-        SendInvitesView.as_view()(request)
+        self.mock_http200_postrequest_htmls(
+            requestkwargs={'data': {'emails': 'invalid'}},
+            cradmin_role=mommy.make('webdemo.Site'))
         self.assertEqual(GenericTokenWithMetadata.objects.count(), 0)
         self.assertEqual(len(mail.outbox), 0)
 
     def test_post_invalid_email_errormessage(self):
-        request = self.factory.post('/test', {
-            'emails': "invalid"
-        })
-        request.cradmin_role = self.testsite
-        response = SendInvitesView.as_view()(request)
-        self.assertEquals(response.status_code, 200)
-        response.render()
-        selector = htmls.S(response.content)
+        mockresponse = self.mock_http200_postrequest_htmls(
+            requestkwargs={'data': {'emails': 'invalid'}},
+            cradmin_role=mommy.make('webdemo.Site'))
         self.assertIn(
             'Invalid email address: invalid',
-            selector.one('form').alltext_normalized)
+            mockresponse.selector.one('form').alltext_normalized)
 
     def test_post_no_emails_no_invite_sent(self):
-        request = self.factory.post('/test', {
-            'emails': ""
-        })
-        request.cradmin_role = self.testsite
         self.assertEqual(GenericTokenWithMetadata.objects.count(), 0)
-        SendInvitesView.as_view()(request)
+        self.mock_http200_postrequest(
+            requestkwargs={'data': {'emails': ''}},
+            cradmin_role=mommy.make('webdemo.Site'))
         self.assertEqual(GenericTokenWithMetadata.objects.count(), 0)
         self.assertEqual(len(mail.outbox), 0)
 
     def test_post_no_emails_errormessage(self):
-        request = self.factory.post('/test', {
-            'emails': "invalid"
-        })
-        request.cradmin_role = self.testsite
-        response = SendInvitesView.as_view()(request)
-        self.assertEquals(response.status_code, 200)
-        response.render()
-        selector = htmls.S(response.content)
+        mockresponse = self.mock_http200_postrequest_htmls(
+            requestkwargs={'data': {'emails': ''}},
+            cradmin_role=mommy.make('webdemo.Site'))
         self.assertIn(
-            'Invalid email address: invalid',
-            selector.one('form').alltext_normalized)
+            'This field is required.',
+            mockresponse.selector.one('form').alltext_normalized)
 
     def test_post_send_invites(self):
-        request = self.factory.post('/test', {
-            'emails': "test1@example.com, test2@example.com"
-        })
-        request.cradmin_role = self.testsite
-        request.cradmin_app = mock.MagicMock()
         self.assertEqual(GenericTokenWithMetadata.objects.count(), 0)
-        response = SendInvitesView.as_view()(request)
-        self.assertEquals(response.status_code, 302)
+        self.mock_http302_postrequest(
+            requestkwargs={'data': {'emails': 'test1@example.com, test2@example.com'}},
+            cradmin_role=mommy.make('webdemo.Site'))
         self.assertEqual(GenericTokenWithMetadata.objects.count(), 2)
         self.assertEqual(len(mail.outbox), 2)
         self.assertEqual(mail.outbox[0].to, [u'test1@example.com'])
