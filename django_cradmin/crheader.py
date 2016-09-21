c58267739a8e9a0fd4a01001b8d8510b9b51ebd3
Espen A. Kristiansen <post@espenak.net>
2016-09-21 17:54:48 +0200

    crheader: Made it easy to change the CSS class of the header.

--- django_cradmin/crheader.py
+++ django_cradmin/crheader.py
@@ -16,9 +16,25 @@ class AbstractHeaderRenderable(renderable.AbstractRenderableWithCss):
     def get_wrapper_htmltag_id(self):
         return 'id_django_cradmin_page_header'
 
+    def include_menutoggle_javascript(self):
+        return False
+
 
 class DefaultHeaderRenderable(AbstractHeaderRenderable):
     template_name = 'django_cradmin/crheader/default-header.django.html'
 
+    def get_bem_block(self):
+        return 'adminui-page-header'
+
+    def get_bem_variants_list(self):
+        return []
+
+    def expand_bem_variants_list(self):
+        bem_block = self.get_bem_block()
+        return ['{}--{}'.format(bem_block, variant) for variant in self.get_bem_variants_list()]
+
     def get_base_css_classes_list(self):
-        return ['adminui-page-header', 'adminui-page-header--floating']
+        return [self.get_bem_block()] + self.expand_bem_variants_list()
+
+    def include_menutoggle_javascript(self):
+        return False
