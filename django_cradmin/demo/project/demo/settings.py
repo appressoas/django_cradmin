066bebdf5053facdd0093fc580b89f340d5010d9
Espen A. Kristiansen <post@espenak.net>
2016-09-30 00:57:49 +0200

    project.demo.settings.DJANGO_CRADMIN_THEME_PATH: Update for new theme structure.

--- django_cradmin/demo/project/demo/settings.py
+++ django_cradmin/demo/project/demo/settings.py
@@ -374,6 +374,6 @@ IEVVTASKS_DEVRUN_RUNNABLES = {
 }
 
 
-DJANGO_CRADMIN_THEME_PATH = 'django_cradmin/{version}/styles/cradmin_theme_full/main.css'.format(
+DJANGO_CRADMIN_THEME_PATH = 'django_cradmin/{version}/styles/basetheme/main.css'.format(
     version=django_cradmin.__version__
 )
