b9c9524a8cf675836d38dffeedfa1cbe2e6d61ee
Espen A. Kristiansen <post@espenak.net>
2016-12-14 23:40:22 +0100

    javascript: Do not load the widgets before the DOM is ready - commit the correct file!

--- django_cradmin/staticsources/django_cradmin/scripts/django_cradmin/django_cradmin_all.js
+++ django_cradmin/staticsources/django_cradmin/scripts/django_cradmin/django_cradmin_all.js
@@ -26,10 +26,10 @@ import DateSelectorHiddenIsoDateWidget from './widgets/DateSelectorHiddenIsoDate
 export default class DjangoCradminAll {
   constructor() {
     new window.ievv_jsbase_core.LoggerSingleton().setDefaultLogLevel(
-      window.ievv_jsbase_core.LOGLEVEL.DEBUG);
-     this.logger = new window.ievv_jsbase_core.LoggerSingleton().getLogger("ievv_jsui_demoapp.DjangoCradminAll");
-     this.logger.setLogLevel(window.ievv_jsbase_core.LOGLEVEL.DEBUG);
-     this.logger.debug(`I am a DjangoCradminAll, and I am aliiiiive!`);
+      window.ievv_jsbase_core.LOGLEVEL.INFO);
+    // this.logger = new window.ievv_jsbase_core.LoggerSingleton().getLogger("ievv_jsui_demoapp.DjangoCradminAll");
+    // this.logger.setLogLevel(window.ievv_jsbase_core.LOGLEVEL.INFO);
+    // this.logger.debug(`I am a DjangoCradminAll, and I am aliiiiive!`);
 
     const widgetRegistry = new window.ievv_jsbase_core.WidgetRegistrySingleton();
     // widgetRegistry.registerWidgetClass('cradmin-datetime-picker', DateTimePicker);
@@ -55,8 +55,22 @@ export default class DjangoCradminAll {
     widgetRegistry.registerWidgetClass('cradmin-dateselector-month', DateSelectorMonthWidget);
     widgetRegistry.registerWidgetClass('cradmin-dateselector-day', DateSelectorDayWidget);
     widgetRegistry.registerWidgetClass('cradmin-dateselector-hidden-iso', DateSelectorHiddenIsoDateWidget);
-    widgetRegistry.initializeAllWidgetsWithinElement(document.body);
+
+    if (document.readyState != 'loading'){
+      widgetRegistry.initializeAllWidgetsWithinElement(document.body);
+    } else {
+      document.addEventListener('DOMContentLoaded', () => {
+        widgetRegistry.initializeAllWidgetsWithinElement(document.body);
+      });
+    }
   }
 }
 
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
 new DjangoCradminAll();
