64a0f78961658b94a1a43f4591ef1302aedbe6b0
Espen A. Kristiansen <post@espenak.net>
2017-01-03 08:24:53 +0100

    uicontainer...fieldset: Update to use the css classes.

--- django_cradmin/uicontainer/fieldset.py
+++ django_cradmin/uicontainer/fieldset.py
@@ -5,9 +5,13 @@ from . import form_mixins
 class FieldSet(AbstractContainerRenderable, form_mixins.FormRenderableChildMixin):
     template_name = 'django_cradmin/uicontainer/fieldset.django.html'
 
-    def __init__(self, title, **kwargs):
+    def __init__(self, title, legend_css_class='legend', **kwargs):
         self.title = title
+        self.legend_css_class = legend_css_class
         super(FieldSet, self).__init__(**kwargs)
 
     def get_default_html_tag(self):
+        return 'fieldset'
+
+    def get_default_bem_block_or_element(self):
         return 'fieldset'
