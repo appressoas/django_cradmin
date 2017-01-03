e0355dab2aa9915cb83556ab8dd44bd02e21543c
Espen A. Kristiansen <post@espenak.net>
2017-01-03 06:42:24 +0100

    uicontainer: Use the new fieldwrapper and help-text css classes.

--- django_cradmin/uicontainer/fieldwrapper.py
+++ django_cradmin/uicontainer/fieldwrapper.py
@@ -138,10 +138,8 @@ class FieldWrapper(container.AbstractContainerRenderable, form_mixins.FormRender
         """
         return self.formrenderable.form[self.fieldname]
 
-    # def get_default_css_classes_list(self):
-    #     css_classes = super(FieldWrapper, self).get_default_css_classes_list()
-    #     css_classes.append('field')
-    #     return css_classes
+    def get_default_bem_block_or_element(self):
+        return 'fieldwrapper'
 
 
 # class NoWrapperElementFieldWrapper(FieldWrapper):
