72ba1b635a0593866a6e8316a134fb7c12b33239
jimma1 <jim@appresso.no>
2017-02-24 08:45:53 +0100

    demo_gettingstarted: Renaming #76

--- django_cradmin/demo/cradmin_gettingstarted/views/create_account_dashboard.py
+++ django_cradmin/demo/cradmin_gettingstarted/views/create_account_dashboard.py
@@ -6,12 +6,12 @@ from django_cradmin.demo.cradmin_gettingstarted.models import AccountAdministrat
 class CreateAccountDashboardView(viewhelpers.generic.WithinRoleTemplateView):
     template_name = 'cradmin_gettingstarted/create_account_dashboard.django.html'
 
-    def __get_account_admin(self):
+    def __get_user(self):
         if self.request.user.is_authenticated:
             user_email = self.request.user.email
             return user_email
 
     def get_context_data(self, **kwargs):
         context = super(CreateAccountDashboardView, self).get_context_data()
-        context['account_admin'] = self.__get_account_admin()
+        context['user'] = self.__get_user()
         return context
