3f3eaae91b36bb4278ae9817550dc51b04ede58b
jimma1 <jim@appresso.no>
2017-02-24 12:08:13 +0100

    demo_gettingstarted: Cleanup #76

--- django_cradmin/demo/cradmin_gettingstarted/crapps/create_account/create_account_view.py
+++ django_cradmin/demo/cradmin_gettingstarted/crapps/create_account/create_account_view.py
@@ -16,41 +16,9 @@ class CreateAccountView(mixins.AccountCreateUpdateMixin, formview.WithinRoleCrea
         )
         account_administrator.full_clean()
         account_administrator.save()
-        print('*'*88)
-        print('Saved account admin')
-        print('*'*88)
-
         return self.new_account
 
     def get_success_url(self):
-        # return self.request.cradmin_app.reverse_appindexurl()
-        print('*'*88)
-        print('Start get success url')
-        print('*'*88)
-        print(self.new_account)
-        print('*'*88)
-        print(self.new_account.id)
-        print('*'*88)
-        print(dir(self.new_account))
-        print('*'*88)
-        print(self.new_account.accountadministrator_set)
-        print('*'*88)
-        account_admin = AccountAdministrator.objects.get(account=self.new_account.id, user=self.request.user)
-        print(dir(account_admin.user))
-        print('*'*88)
-        print('Account admin user: {}'.format(account_admin.user.email))
-        print('*'*88)
-        print('Cradmin role: {}'.format(self.request.cradmin_role))
-        print('*'*88)
-        print('Cradmin app: {}'.format(self.request.cradmin_app))
-        print('*'*88)
-        print('Cradmin instance: {}'.format(self.request.cradmin_instance))
-        print('*'*88)
-        print('User: {}'.format(self.request.user.email))
-
-
-
-
         return reverse_cradmin_url(
             instanceid='gettingstarted',
             appname='account_admin',
