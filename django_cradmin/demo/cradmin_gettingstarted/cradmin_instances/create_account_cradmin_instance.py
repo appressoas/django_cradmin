6b870bb666f72696b14ff2d8345f45f964e22250
jimma1 <jim@appresso.no>
2017-02-26 07:43:10 +0100

    demo_gettingstarted: Add links on pages #76

--- django_cradmin/demo/cradmin_gettingstarted/cradmin_instances/create_account_cradmin_instance.py
+++ django_cradmin/demo/cradmin_gettingstarted/cradmin_instances/create_account_cradmin_instance.py
@@ -1,15 +1,14 @@
-from django.http import Http404
-
 from django_cradmin import crinstance
+from django_cradmin import crmenu
 from django_cradmin.demo.cradmin_gettingstarted.crapps import create_account
-
+from django.utils.translation import ugettext_lazy
 
 class CreateAccountCrAdminInstance(crinstance.NoRoleMixin, crinstance.BaseCrAdminInstance):
     id = 'create_account'
     rolefrontpage_appname = 'dashboard'
 
     apps = [
-        ('dashboard', create_account.App),
+        ('dashboard', create_account.App)
     ]
 
     def has_access(self):
