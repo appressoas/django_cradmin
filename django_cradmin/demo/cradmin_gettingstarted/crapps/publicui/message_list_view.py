8da7e798da17b3cdfb0ae050e1b5ff494912fdcb
jimma1 <jim@appresso.no>
2017-03-01 10:24:31 +0100

    demo_gettingstarted: Make ok display of messages on tempalte when not using list view#76

--- django_cradmin/demo/cradmin_gettingstarted/crapps/publicui/message_list_view.py
+++ django_cradmin/demo/cradmin_gettingstarted/crapps/publicui/message_list_view.py
@@ -5,7 +5,7 @@ from django_cradmin.demo.cradmin_gettingstarted.models import Message
 class MessageListView(viewhelpers.generic.WithinRoleTemplateView):
     """View for showing messages in publicui"""
 
-    template_name = 'cradmin_gettingstarted/publicui/message_list.django.html'
+    template_name = 'cradmin_gettingstarted/crapps/publicui/message_list.django.html'
 
     def get_context_data(self, **kwargs):
         context = super(MessageListView, self).get_context_data(**kwargs)
