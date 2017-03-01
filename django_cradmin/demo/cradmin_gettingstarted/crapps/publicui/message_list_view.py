15899ad159d2018d7bc027bcdac303662f4ccf42
jimma1 <jim@appresso.no>
2017-03-01 14:39:49 +0100

    demo_gettingstarted: Create code and files for customization of listbuilder and listbuilderview with own templates #76

--- django_cradmin/demo/cradmin_gettingstarted/crapps/publicui/message_list_view.py
+++ django_cradmin/demo/cradmin_gettingstarted/crapps/publicui/message_list_view.py
@@ -1,4 +1,3 @@
-from django_cradmin import viewhelpers
 from django_cradmin.crinstance import reverse_cradmin_url
 from django_cradmin.demo.cradmin_gettingstarted.models import Message
 from django_cradmin.viewhelpers import listbuilder, listbuilderview
@@ -8,9 +7,7 @@ class MessageItemValue(listbuilder.itemvalue.TitleDescription):
     """Get values for items in the list"""
 
     valuealias = 'message'
-
-    def get_title(self):
-        return self.message.title
+    template_name = 'cradmin_gettingstarted/crapps/publicui/message_listbuilder.django.html'
 
     def get_description(self):
         return self.message.body
@@ -26,9 +23,9 @@ class MessageItemFrameLink(listbuilder.itemframe.Link):
 class MessageListBuilderView(listbuilderview.View):
     """Builds the list for the view"""
     model = Message
-    # value_renderer_class = MessageItemValue
+    value_renderer_class = MessageItemValue
     # frame_renderer_class = MessageItemFrameLink
-    # template_name = 'cradmin_gettingstarted/crapps/publicui/message_list.django.html'
+    template_name = 'cradmin_gettingstarted/crapps/publicui/message_listbuilder_view.django.html'
     # paginate_by = 1
 
     def get_queryset_for_role(self):
