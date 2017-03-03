082a1714d20351a0cc754fd43e6179045bcc55cc
jimma1 <jim@appresso.no>
2017-03-03 10:29:28 +0100

    demo_gettingstarted: Add cradmin reverse url for success deletion of an account code, test the code and update guide  #76

--- django_cradmin/demo/cradmin_gettingstarted/crapps/account_adminui/delete_account_view.py
+++ django_cradmin/demo/cradmin_gettingstarted/crapps/account_adminui/delete_account_view.py
@@ -1,3 +1,4 @@
+from django_cradmin.crinstance import reverse_cradmin_url
 from django_cradmin.demo.cradmin_gettingstarted.models import Account
 from django_cradmin.viewhelpers import formview
 
@@ -13,3 +14,6 @@ class AccountDeleteView(formview.WithinRoleDeleteView):
 
     def get_queryset_for_role(self):
         return Account.objects.filter(id=self.request.cradmin_role.pk)
+
+    def get_success_url(self):
+        return reverse_cradmin_url(instanceid='create_account', appname='dashboard')
--- django_cradmin/demo/cradmin_gettingstarted/tests/test_crapps/test_account_adminui/test_delete_account_view.py
+++ django_cradmin/demo/cradmin_gettingstarted/tests/test_crapps/test_account_adminui/test_delete_account_view.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from model_mommy import mommy
 
 from django_cradmin import cradmin_testhelpers
+from django_cradmin import crinstance
 from django_cradmin.demo.cradmin_gettingstarted.crapps.account_adminui import delete_account_view
 from django_cradmin.demo.cradmin_gettingstarted.models import Account
 
@@ -48,3 +49,43 @@ class TestDeleteAccountView(TestCase, cradmin_testhelpers.TestCaseMixin):
         )
         self.assertFalse(Account.objects.filter(name='Delete me'))
         self.assertEqual(10, Account.objects.count())
+
+    def test_success_url_after_delete_when_one_account(self):
+        account = mommy.make(
+            'cradmin_gettingstarted.Account',
+            name='Delete me'
+        )
+        mommy.make(
+            'cradmin_gettingstarted.AccountAdministrator',
+            account=account,
+            user=mommy.make(settings.AUTH_USER_MODEL)
+        )
+        mockresponse = self.mock_http302_postrequest(cradmin_role=account)
+        self.assertEqual(mockresponse.response['location'],
+                         crinstance.reverse_cradmin_url(instanceid='create_account',
+                                                        appname='dashboard'))
+
+    def test_success_url_after_delete_when_three_accounts_for_one_admin(self):
+        account_one = mommy.make('cradmin_gettingstarted.Account')
+        account_two = mommy.make('cradmin_gettingstarted.Account')
+        account_three = mommy.make('cradmin_gettingstarted.Account')
+        user = mommy.make(settings.AUTH_USER_MODEL)
+        mommy.make(
+            'cradmin_gettingstarted.AccountAdministrator',
+            account=account_one,
+            user=user
+        )
+        mommy.make(
+            'cradmin_gettingstarted.AccountAdministrator',
+            account=account_two,
+            user=user
+        )
+        mommy.make(
+            'cradmin_gettingstarted.AccountAdministrator',
+            account=account_three,
+            user=user
+        )
+        mockresponse = self.mock_http302_postrequest(cradmin_role=account_two)
+        self.assertEqual(mockresponse.response['location'],
+                         crinstance.reverse_cradmin_url(instanceid='create_account',
+                                                        appname='dashboard'))
