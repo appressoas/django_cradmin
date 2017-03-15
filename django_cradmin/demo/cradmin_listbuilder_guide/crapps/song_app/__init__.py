f3404d5553ec53900a6310ba931f352f42cfe768
jimma1 <jim@appresso.no>
2017-03-15 16:32:56 +0100

    demo_listbuilder_guide: Work in progress #76

--- django_cradmin/demo/cradmin_listbuilder_guide/crapps/song_app/__init__.py
+++ django_cradmin/demo/cradmin_listbuilder_guide/crapps/song_app/__init__.py
@@ -1,4 +1,5 @@
 from django_cradmin import crapp
+from django_cradmin.demo.cradmin_listbuilder_guide.crapps.song_app import song_create_view
 from django_cradmin.demo.cradmin_listbuilder_guide.crapps.song_app import song_dashboard
 
 
@@ -8,5 +9,10 @@ class App(crapp.App):
             r'^$',
             song_dashboard.SingleDashboardView.as_view(),
             name=crapp.INDEXVIEW_NAME
+        ),
+        crapp.Url(
+            r'^create$',
+            song_create_view.SongCreateView.as_view(),
+            name='create'
         )
     ]
---
+++ django_cradmin/demo/cradmin_listbuilder_guide/crapps/song_app/song_create_view.py
@@ -0,0 +1,28 @@
+from django_cradmin import uicontainer
+from django_cradmin import viewhelpers
+from django_cradmin.demo.cradmin_listbuilder_guide.models import Song
+
+
+class SongCreateView(viewhelpers.formview.WithinRoleCreateView):
+    model = Song
+    roleid_field = 'artist'
+    fields = [
+        'title',
+        'album'
+    ]
+
+    def get_form_renderable(self):
+        return uicontainer.layout.AdminuiPageSectionTight(
+            children=[
+                uicontainer.form.Form(
+                    form=self.get_form(),
+                    children={
+                        uicontainer.foreignkeyfield.Dropdown('album'),
+                        uicontainer.fieldwrapper.FieldWrapper('title'),
+                        uicontainer.button.SubmitPrimary(
+                            text='Save'
+                        )
+                    }
+                )
+            ]
+        ).bootstrap()
