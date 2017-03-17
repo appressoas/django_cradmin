bdf40b571e38754e490a96877a849f9e6757e17a
jimma1 <jim@appresso.no>
2017-03-17 09:59:58 +0100

    demo_listbuilder_guide: Add edit view for Album which passes sanity tests #76

--- django_cradmin/demo/cradmin_listbuilder_guide/crapps/album_app/__init__.py
+++ django_cradmin/demo/cradmin_listbuilder_guide/crapps/album_app/__init__.py
@@ -1,6 +1,7 @@
 from django_cradmin import crapp
 from django_cradmin.demo.cradmin_listbuilder_guide.crapps.album_app import album_create_view
 from django_cradmin.demo.cradmin_listbuilder_guide.crapps.album_app import album_dashboard
+from django_cradmin.demo.cradmin_listbuilder_guide.crapps.album_app import album_edit_view
 
 
 class App(crapp.App):
@@ -11,8 +12,13 @@ class App(crapp.App):
             name=crapp.INDEXVIEW_NAME
         ),
         crapp.Url(
-            r'^create',
+            r'^create$',
             album_create_view.AlbumCreateView.as_view(),
             name='create'
+        ),
+        crapp.Url(
+            r'^edit/(?P<pk>\d+)$',
+            album_edit_view.AlbumEditView.as_view(),
+            name='edit'
         )
     ]
--- django_cradmin/demo/cradmin_listbuilder_guide/crapps/album_app/album_create_view.py
+++ django_cradmin/demo/cradmin_listbuilder_guide/crapps/album_app/album_create_view.py
@@ -1,30 +1,8 @@
-from django_cradmin import uicontainer
 from django_cradmin import viewhelpers
-from django_cradmin.demo.cradmin_listbuilder_guide.models import Album
+from django_cradmin.demo.cradmin_listbuilder_guide.crapps.album_app.album_mixins import AlbumCreateUpdateMixin
 
 
-class AlbumCreateView(viewhelpers.formview.WithinRoleCreateView):
-    model = Album
-    roleid_field = 'artist'
-    fields = [
-        'title'
-    ]
-
-    def get_form_renderable(self):
-        """return the form which to render"""
-        return uicontainer.layout.AdminuiPageSectionTight(
-            children=[
-                uicontainer.form.Form(
-                    form=self.get_form(),
-                    children=[
-                        uicontainer.fieldwrapper.FieldWrapper('title'),
-                        uicontainer.button.SubmitPrimary(
-                            text='Save'
-                        )
-                    ]
-                )
-            ]
-        ).bootstrap()
+class AlbumCreateView(AlbumCreateUpdateMixin, viewhelpers.formview.WithinRoleCreateView):
 
     def save_object(self, form, commit=True):
         new_album = super(AlbumCreateView, self).save_object(form, commit)
