44fbf75d5c4ee57d70dcaded30256a4ed42d3910
jimma1 <jim@appresso.no>
2017-03-20 14:11:03 +0100

    demo_listbuilder_guide: Add expandable menu to CRadmin instance #76

--- django_cradmin/demo/cradmin_listbuilder_guide/listbuilder_crinstance.py
+++ django_cradmin/demo/cradmin_listbuilder_guide/listbuilder_crinstance.py
@@ -1,4 +1,7 @@
+from django.utils.translation import ugettext_lazy
+
 from django_cradmin import crinstance
+from django_cradmin import crmenu
 from django_cradmin.demo.cradmin_listbuilder_guide.crapps import edit_delete_app
 from django_cradmin.demo.cradmin_listbuilder_guide.crapps import edit_delete_preview_app
 from django_cradmin.demo.cradmin_listbuilder_guide.crapps import focus_box_app
@@ -12,7 +15,7 @@ class ListbuilderCradminInstance(crinstance.BaseCrAdminInstance):
     rolefrontpage_appname = 'songs'
     apps = [
         ('songs', edit_delete_app.App),
-        ('preview_app', edit_delete_preview_app.App),
+        ('preview', edit_delete_preview_app.App),
         ('focus_box', focus_box_app.App)
     ]
 
@@ -24,3 +27,22 @@ class ListbuilderCradminInstance(crinstance.BaseCrAdminInstance):
         if not self.request.user.is_superuser:
             queryset = queryset.filter(albumadministrator__user=self.request.user)
         return queryset
+
+    def get_expandable_menu_item_renderables(self):
+        return [
+            crmenu.ExpandableMenuItem(
+                label=ugettext_lazy('Edit Delete Demo'),
+                url=self.appindex_url('songs'),
+                is_active=self.request.cradmin_app.appname == 'songs'
+            ),
+            crmenu.ExpandableMenuItem(
+                label=ugettext_lazy('Edit Delete Preview Demo'),
+                url=self.appindex_url('preview'),
+                is_active=self.request.cradmin_app.appname == 'preview'
+            ),
+            crmenu.ExpandableMenuItem(
+                label=ugettext_lazy('Focus Box Demo'),
+                url=self.appindex_url('focus_box'),
+                is_active=self.request.cradmin_app.appname == 'focus_box'
+            )
+        ]
