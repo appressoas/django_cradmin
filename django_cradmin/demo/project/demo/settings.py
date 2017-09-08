377d27e8d5319e9b04e90b9a38f143d3944da992
Espen A. Kristiansen <post@espenak.net>
2017-09-08 17:19:05 +0200

    Set ALLOWED_HOST for demo.

--- django_cradmin/demo/project/demo/settings.py
+++ django_cradmin/demo/project/demo/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = 'y%j0x%=7a^sf53m*s^5nbmfe0_t13d*oibfx#m#*wz1x+k6+m1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'cradmin.dev',
+]
 
 
 # Application definition
