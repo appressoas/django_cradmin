cf0a790517e62942db3412f4cbb34675dcba6765
Espen A. Kristiansen <t@espenak.net>
2018-02-27 21:03:34 +0100

    Remove the old widgets.filewidgets.

--- django_cradmin/demo/webdemo/crapps/pages/editviews.py
+++ django_cradmin/demo/webdemo/crapps/pages/editviews.py
@@ -20,38 +20,6 @@ class PageCreateUpdateMixin(object):
         'internal_notes'
     ]
 
-    # def get_preview_url(self):
-    #     return self.request.cradmin_app.reverse_appurl('preview')
-    #
-    # def _get_image_selectview_url(self):
-    #     return self.request.cradmin_instance.reverse_url('imagearchive', 'singleselect')
-
-    # def get_form(self, form_class=None):
-    #     form = super(PageCreateUpdateMixin, self).get_form(form_class=form_class)
-    #     # form.fields['body'].widget = WysiHtmlTextArea(attrs={})
-    #     form.fields['body'].widget = AceMarkdownWidget()
-    #     preview = '<p class="text-muted">(No image selected)</p>'
-    #     if self.object and self.object.image:
-    #         preview = self.object.image.get_preview_html(request=self.request)
-    #     form.fields['image'].widget = ModelChoiceWidget(
-    #         queryset=ArchiveImage.objects.filter_owned_by_role(self.request.cradmin_role),
-    #         preview=preview,
-    #         selectview_url=self._get_image_selectview_url()
-    #     )
-    #     form.fields['publishing_time'].widget = DateTimePickerWidget(
-    #         # minimum_datetime=datetime(2014, 8, 14, 12, 30),
-    #         minimum_datetime=timezone.now(),
-    #         maximum_datetime=datetime(2030, 12, 5, 21, 40),
-    #         required=False
-    #     )
-    #     form.fields['unpublish_time'].widget = DatePickerWidget(
-    #         no_value_preview_text='No date selected')
-    #     # form.fields['unpublish_time'].widget = DateTimePickerWidget(
-    #     #     no_value_preview_text='No date selected')
-    #     form.fields['attachment'].widget = filewidgets.ImageWidget(request=self.request)
-    #     # form.fields['attachment'].widget = filewidgets.FileWidget()
-    #     return form
-
     def get_form_renderable(self):
         return uicontainer.layout.AdminuiPageSectionTight(
             children=[
