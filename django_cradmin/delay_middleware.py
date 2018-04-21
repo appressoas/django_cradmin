50ffc32417f442d865d66def35e3353a15628610
Espen A. Kristiansen <t@espenak.net>
2018-04-21 11:49:41 +0200

    delay_middleware: Update for new style middleware format.

--- django_cradmin/delay_middleware.py
+++ django_cradmin/delay_middleware.py
@@ -11,6 +11,11 @@ class DelayMiddleware(object):
     - Set ``DJANGO_CRADMIN_DELAY_MIDDLEWARE_MILLISECONDS`` to the number of milliseconds
       delay you want to add to all requests (I.E.: 2000 for 2 seconds).
     """
-    def process_request(self, request):
-        time.sleep(settings.DJANGO_CRADMIN_DELAY_MIDDLEWARE_MILLISECONDS / 1000.0)
-        return None
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        delay_seconds = getattr(settings, 'DJANGO_CRADMIN_DELAY_MIDDLEWARE_MILLISECONDS', None)
+        if delay_seconds:
+            time.sleep(delay_seconds / 1000.0)
+        return self.get_response(request)
