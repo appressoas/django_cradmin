a039d318c42b960a838457602216c11cff4e215a
Espen A. Kristiansen <t@espenak.net>
2018-05-20 05:43:37 +0200

    Release django_cradmin_js 4.0.0-beta.2.

--- django_cradmin/apps/django_cradmin_js/staticsources/django_cradmin_js/webpack.config.js
+++ django_cradmin/apps/django_cradmin_js/staticsources/django_cradmin_js/webpack.config.js
@@ -1,28 +1,11 @@
 const path = require('path')
 const appconfig = require('./ievv_buildstatic.appconfig.json')
+const webpackConfig = require('ievv_jsbase/webpackHelpers/webpackConfig')
 
-module.exports = {
-  entry: [
-    '@babel/polyfill',
-    path.resolve(__dirname, 'source/django_cradmin_all.js')
-  ],
-  output: {
-    filename: 'django_cradmin_all.js',
-    path: appconfig.destinationfolder
-  },
+const config = new webpackConfig.WebpackConfig(appconfig.destinationfolder)
+config.enableBabel()
+config.addEntry('django_cradmin_all', [
+  path.resolve(__dirname, 'source/django_cradmin_all.js')
+])
 
-  resolve: {
-    extensions: ['.js', '.jsx', '.json']
-  },
-
-  module: {
-    rules: [
-      {
-        test: /.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        include: [path.resolve(__dirname, 'source')]
-      }
-    ]
-  }
-}
+module.exports = config.generate()
