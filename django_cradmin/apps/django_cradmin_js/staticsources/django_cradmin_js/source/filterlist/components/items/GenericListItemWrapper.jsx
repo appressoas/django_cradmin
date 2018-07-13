ed6a0a05a5d3a6e1448aa2dcfc695c68b4a60310
Espen A. Kristiansen <t@espenak.net>
2018-07-13 20:20:36 +0200

    cradmin_js...filterlist: Add new GenericListItemWrapper class.

---
+++ django_cradmin/apps/django_cradmin_js/staticsources/django_cradmin_js/source/filterlist/components/items/GenericListItemWrapper.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import AbstractListItem from './AbstractListItem'
+
+export default class GenericListItemWrapper extends AbstractListItem {
+  static get propTypes () {
+    return Object.assign(super.propTypes, {
+      componentType: PropTypes.func.isRequired,
+      componentProps: PropTypes.object,
+      listItemIdName: PropTypes.string.isRequired
+    })
+  }
+
+  static get defaultProps () {
+    return Object.assign(super.defaultProps, {
+      componentType: null,
+      componentProps: {},
+      listItemIdName: null
+    })
+  }
+
+  render () {
+    const Type = this.props.componentType
+    const props = Object.assign({
+      [this.props.listItemIdName]: this.props.listItemId
+    }, this.props.componentProps)
+    return <Type {...props} />
+  }
+}
