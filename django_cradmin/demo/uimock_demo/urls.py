b43c6604163e18ae03c6ef206c4892e0beb873f7
Espen A. Kristiansen <t@espenak.net>
2020-11-12 11:45:41 +0100

    Fix url that was wrongly converted to django3.

--- django_cradmin/demo/uimock_demo/urls.py
+++ django_cradmin/demo/uimock_demo/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from django_cradmin import viewhelpers
 
 from .views import overview
 
 urlpatterns = [
-    path('simple/<str:mockname>',
-         viewhelpers.uimock.UiMock.as_view(template_directory='uimock_demo/simple/'),
-         name='cradmin_uimock_demo_simple'),
+    re_path(r'^simple/(?P<mockname>.+)?$',
+            viewhelpers.uimock.UiMock.as_view(template_directory='uimock_demo/simple/'),
+            name='cradmin_uimock_demo_simple'),
     path('',
          overview.Overview.as_view(),
          name='cradmin_uimock_demo'),
