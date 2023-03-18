dc82ffe838f787960d35dba113ae8b93c0d79c06
Espen Kristiansen <espen@appresso.no>
2023-03-18 21:37:01 +0100

    Update to ievv-opensource 8.2.1.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'Django>=3.2.0,<4.0.0',
         'html2text',
-        'ievv_opensource>=8.0.0,<9.0.0'
+        'ievv_opensource>=8.2.1,<9.0.0'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
