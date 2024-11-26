23beb9ac125ac8c315bcf584550c987bc5f29cf8
Jim Nordal <jim@appresso.no>
2024-11-26 12:25:38 +0100

    fix(virtualenv): install/upgrade pip, setuptools and wheel

--- tools/recreate-virtualenv.sh
+++ tools/recreate-virtualenv.sh
@@ -6,4 +6,4 @@ fi
 
 python -m venv .venv || { echo 'FAILED: python -m venv .venv' ; exit 1; };
 
-.venv/bin/python -m pip install --upgrade pip || { echo 'FAILED: .venv/bin/python -m pip install --upgrade pip' ; exit 1 ; };
+.venv/bin/python -m pip install -v --upgrade pip setuptools wheel || { echo 'FAILED: .venv/bin/python -m pip install --upgrade pip setuptools wheel' ; exit 1 ; };
