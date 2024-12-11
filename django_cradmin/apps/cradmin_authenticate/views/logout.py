05d4da2e44df89b88f0b67244d2605c2b9a7f493
Espen Kristiansen <espen@appresso.no>
2024-12-11 13:55:01 +0100

    fix: Allow GET requests in logout view.
    Broken after django5 update.

--- django_cradmin/apps/cradmin_authenticate/views/logout.py
+++ django_cradmin/apps/cradmin_authenticate/views/logout.py
@@ -3,7 +3,8 @@ from django.contrib.auth import views as auth_views
 
 
 class CradminLogoutView(auth_views.LogoutView):
-    template_name = 'cradmin_authenticate/logout.django.html'
+    http_method_names = ["post", "options", "get"]
+    template_name = "cradmin_authenticate/logout.django.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -11,5 +12,7 @@ class CradminLogoutView(auth_views.LogoutView):
         return context
 
 
-def cradmin_logoutview(request, template_name='cradmin_authenticate/logout.django.html'):
+def cradmin_logoutview(
+    request, template_name="cradmin_authenticate/logout.django.html"
+):
     return CradminLogoutView.as_view(template_name=template_name)(request)
